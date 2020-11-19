8152cb292f2e579be67c0ac9b34dba9e20495afc
Matt Stauffer <matt@tighten.co>
2020-11-19 18:00:33 -0500

    Wip

--- cache/907171cd6dd4e5a374219f98e8586fff5b4c08a8.php
+++ cache/907171cd6dd4e5a374219f98e8586fff5b4c08a8.php
@@ -1,4 +1,4 @@
-@php
+<?php
 $categories = [
     'Laravel Command-line tools' => [
         'Laravel Installer' => 'yay',
@@ -91,17 +91,18 @@ $categories = [
         'Invoker' => 'dunno',
     ],
 ];
-@endphp
+?>
 
-@foreach ($categories as $name => $tools)
-    <h2>{{ $name }}</h2>
+<?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $name => $tools): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <h2><?php echo e($name); ?></h2>
     <ul>
-    @foreach ($tools as $toolName => $partial)
-        @if (is_array($partial))
-            <li><a href="{{ $partial['link'] }}">@include('_partials.' . $partial['status']){{ $toolName }}</a></li>
-        @else
-            <li>@include('_partials.' . $partial){{ $toolName }}</li>
-        @endif
-    @endforeach
+    <?php $__currentLoopData = $tools; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $toolName => $partial): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <?php if(is_array($partial)): ?>
+            <li><a href="<?php echo e($partial['link']); ?>"><?php echo $__env->make('_partials.' . $partial['status'], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php echo e($toolName); ?></a></li>
+        <?php else: ?>
+            <li><?php echo $__env->make('_partials.' . $partial, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php echo e($toolName); ?></li>
+        <?php endif; ?>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     </ul>
-@endforeach
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php /**PATH /Users/mattstauffer/Sites/laravelm1/source/_partials/status.blade.php ENDPATH**/ ?>
