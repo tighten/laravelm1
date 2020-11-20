7543b454540e67cfc647cb859670b5f9c29b5636
Matt Stauffer <matt@tighten.co>
2020-11-19 22:10:04 -0500

    Wip

--- source/_partials/status.blade.php
+++ source/_partials/status.blade.php
@@ -90,7 +90,7 @@ $categories = [
             <div class="inline-block" x-data="{ open: false }">
                 <a @click="open = true" class="cursor-pointer">[i]</a>
                 <div
-                    class="absolute"
+                    class="absolute bg-white border border-gray-500 p-4"
                     x-show="open"
                     @click.away="open = false"
                 >
