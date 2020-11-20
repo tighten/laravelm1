3b0f08de0e34f4552fd016503231a2cc1fb83aff
Matt Stauffer <matt@tighten.co>
2020-11-20 09:19:27 -0500

    Wip

--- source/_partials/live-blog.blade.php
+++ source/_partials/live-blog.blade.php
@@ -105,5 +105,5 @@ released but not yet supported version.
         <li>The way I connected to Postgres on TablePlus doesn't work unless there's a database table named the same as my computer user, and since I didn't feel like taking the time to figure out why, I just ran <code>psql postgres</code> and in that REPL I wrote <code>CREATE DATABASE matthewstauffer;</code> and then hit enter. Once that was done, I could connect to this Postgres database using the default settings from TablePlus. Good to go!</li>
         <li>OK, just checked out a ton of GUI apps that all work fine. Gonna call it for a while.</li>
         <li><span>Thursday 10:13PM</span>Lied. Tried OBS. Doesn't work.</li>
-        <li><span>Friday 9:19AM</span>OK, got a few minutes free. What's next?</li>
+        <li><span>Friday 9:19AM</span>OK, got a few minutes free. What's next? I can't try Horizon without Redis. </li>
     </ul>
