619e74460b4ec1f1c9f34b35ab9c1f6bb63c8456
Matt Stauffer <matt@tighten.co>
2020-11-19 21:39:47 -0500

    Wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,6 +6,7 @@
         <div class="mt-8">
             <h1 id="intro-docs-template">{{ $page->siteName }}</h1>
 
+
             <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
 {{--             <div class="flex my-10">
                 <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
