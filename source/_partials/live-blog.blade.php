2822c60467076723c1628fc98b3a6b09fdb9634c
Matt Stauffer <matt@tighten.co>
2020-11-20 09:25:29 -0500

    Wip

--- source/_partials/live-blog.blade.php
+++ source/_partials/live-blog.blade.php
@@ -107,5 +107,6 @@ released but not yet supported version.
         <li><span>Thursday 10:13PM</span>Lied. Tried OBS. Doesn't work.</li>
         <li><span>Friday 9:19AM</span>OK, got a few minutes free. What's next? I can't try Horizon without Redis. Homestead is a big lift, so I'm gonna wait on that. Memcached! Let's go!</li>
         <li><code>arm brew install memcached && brew services start memcached</code>... ran great. Now I have to remember how to test memcached. Honestly, using it for a Laravel app's cache is probably easiest.</li>
+        <li>Whoops, now I remember! You have to install the memcached extension using PECL. Let's test PECL!</li>
         <li>
     </ul>
