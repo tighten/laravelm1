86a650217d17fc067f385b773ea9b7ffec727dc5
Matt Stauffer <matt@tighten.co>
2020-11-19 21:40:00 -0500

    Wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,15 +4,9 @@
 <section class="container max-w-6xl mx-auto px-6 py-10 md:py-12">
     <div class="flex flex-col-reverse mb-8 lg:flex-row lg:mb-12">
         <div class="mt-8">
-            <h1 id="intro-docs-template">{{ $page->siteName }}</h1>
+            <h1 >{{ $page->siteName }}</h1>
 
-
-            <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
-{{--             <div class="flex my-10">
-                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
-
-                <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About Jigsaw</a>
-            </div> --}}
+            <h2 class="font-light mt-4">{{ $page->siteDescription }}</h2>
 
             <p class="text-lg">Green is yes; red is no; grey hasn't been tested yet.</p>
             <p>Source here, in case you want to make PRs: <a href="https://github.com/tighten/laravelm1">github.com/tighten/laravelm1</a></p>
