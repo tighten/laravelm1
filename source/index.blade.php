bf53c06bf28431cdf3a13053d6b238d38354bb44
Matt Stauffer <matt@tighten.co>
2020-11-19 21:20:51 -0500

    Wip

--- source/index.blade.php
+++ source/index.blade.php
@@ -23,10 +23,10 @@
         <h4>Legend</h4>
 
         <table class="border border-gray-400">
-            <tr><td class="pl-3 pr-4 text-right">Haven't tested yet</td><td class="pr-3">@status('unsure')</td></tr>
-            <tr><td class="pl-3 pr-4 text-right">Doesn't work as of testing</td><td class="pr-3">@status('no')</td></tr>
-            <tr><td class="pl-3 pr-4 text-right">Works, through Rosetta 2</td><td class="pr-3">@status('rosetta')</td></tr>
-            <tr><td class="pl-3 pr-4 text-right">Works natively</td><td class="pr-3">@status('yes')</td></tr>
+            <tr><td class="py-2 pl-3 pr-4 text-right">Haven't tested yet</td><td class="py-2 pr-3">@status('unsure')</td></tr>
+            <tr><td class="py-2 pl-3 pr-4 text-right">Doesn't work as of testing</td><td class="py-2 pr-3">@status('no')</td></tr>
+            <tr><td class="py-2 pl-3 pr-4 text-right">Works, through Rosetta 2</td><td class="py-2 pr-3">@status('rosetta')</td></tr>
+            <tr><td class="py-2 pl-3 pr-4 text-right">Works natively</td><td class="py-2 pr-3">@status('yes')</td></tr>
         </table>
     </div>
 
