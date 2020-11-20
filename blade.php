ae69f9314269a8d0b085493e86320be2bca79746
Matt Stauffer <matt@tighten.co>
2020-11-19 19:37:00 -0500

    Wip

--- blade.php
+++ blade.php
@@ -4,6 +4,8 @@ return [
     'status' => function ($expression, $page) {
         $colors = [
             'unsure' => 'bg-gray-400',
+            'no' => 'bg-red-500',
+
         ];
 
         return '<div class="' . $colors[$expression] . ' inline-block w-3 h-3 rounded mr-1"></div>'
