ee0a69478f98d12f5626ff750f7baad89232f3e1
Matt Stauffer <matt@tighten.co>
2021-06-15 16:00:56 -0400

    Add ARMShaming

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,38 +1,42 @@
 @extends('_layouts.master')
 
 @section('body')
-<section class="container max-w-6xl mx-auto px-6 py-10 md:py-12">
+<section class="container max-w-6xl px-6 py-10 mx-auto md:py-12">
     <div class="flex flex-col-reverse mb-8 lg:flex-row lg:mb-12">
         <div class="mt-8">
-            <h1 >{{ $page->siteName }}</h1>
+            <h1>{{ $page->siteName }}</h1>
+            <p class="mt-2">Last update: {{ date('M j, Y') }}</p>
 
-            <h2 class="font-light mt-4">{{ $page->siteDescription }}</h2>
+            <p>Documenting the experience of working on an m1 Apple computer as a Laravel programmer.</p>
 
-            <p>Source here, in case you want to make PRs: <a href="https://github.com/tighten/laravelm1">github.com/tighten/laravelm1</a></p>
-            <p>Another resource here: <a href="https://roaringapps.com/collections/list-of-apple-silicon-native-apps">https://roaringapps.com/collections/list-of-apple-silicon-native-apps</a></p>
-            <p>Another here, which has a very similar style to how I've done this but much nicer-looking and more comprehensive: <a href="https://isapplesiliconready.com/">https://isapplesiliconready.com/</a></p>
+            <h3>Links</h3>
+            <ul class="ml-6 list-disc">
+                <li><a href="https://armshaming.space/">ARM Shaming:</a> listing all Docker repos that don't work on m1</li>
+                <li><a href="https://isapplesiliconready.com/">Is Apple Silicon Ready?</a> Broader list of apps ready for m1</li>
+                <li><a href="https://github.com/tighten/laravelm1">Source Code</a> for this site</li>
+            </ul>
         </div>
     </div>
 
     <div>
         <h4>Legend</h4>
 
-        <table class="border border-gray-400">
+        <table class="border border-2 border-gray-400">
             <tr>
                 <td class="py-1 pl-3">@status('unsure')</td>
-                <td class="py-1 pr-3 pl-3">Haven't tested yet</td>
+                <td class="py-1 pl-3 pr-3">Haven't tested yet</td>
             </tr>
             <tr>
                 <td class="py-1 pl-3">@status('no')</td>
-                <td class="py-1 pr-3 pl-3">Doesn't work as of testing</td>
+                <td class="py-1 pl-3 pr-3">Doesn't work as of testing</td>
             </tr>
             <tr>
                 <td class="py-1 pl-3">@status('rosetta')</td>
-                <td class="py-1 pr-3 pl-3">Works, through Rosetta 2</td>
+                <td class="py-1 pl-3 pr-3">Works, through Rosetta 2</td>
             </tr>
             <tr>
                 <td class="py-1 pl-3">@status('yes')</td>
-                <td class="py-1 pr-3 pl-3">Works natively</td>
+                <td class="py-1 pl-3 pr-3">Works natively</td>
             </tr>
         </table>
     </div>
